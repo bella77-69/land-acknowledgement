28e5076bfe22e8b556ac50a898601c92c0e888e2
chantelle mckenzie <mrspasceri@hotmail.com>
2023-09-26 19:46:50 -0700

    updated LandAcknowledgement.js page, added a button so the user can change the Land Acknowledgement, just added different variations you could write one

--- src/pages/LandAcknowledgement/LandAcknowledgement.js
+++ src/pages/LandAcknowledgement/LandAcknowledgement.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LandAcknowledgement.css";
 
 const LandAcknowledgment = ({ indigenousLands }) => {
+  const [currentAcknowledgmentIndex, setCurrentAcknowledgmentIndex] = useState(0);
 
   const btnLearn = (e) => {
     e.preventDefault();
     window.location.href = "/learn-more";
+  };
+
+  const indigenousLandVariations = [
+    "I am honored to be a guest on the traditional and unceded territories of the ",
+    "I acknowledge that I am on the traditional and unceded territories of the ",
+    "Today, we gather on the ancestral lands of the  ",
+    "We are grateful to gather on the traditional and unceded territories of the ",
+    "We are gathered on the traditional and unceded territories of the ",
+    "We are meeting on the traditional and unceded territories of the ",
+    
+  ];
+
+  const changeAcknowledgment = () => {
+    // Increment the current acknowledgment index and loop back to the first one if needed
+    setCurrentAcknowledgmentIndex((prevIndex) =>
+      prevIndex === indigenousLandVariations.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
@@ -14,7 +32,7 @@ const LandAcknowledgment = ({ indigenousLands }) => {
       <div className="content">
         {indigenousLands.length > 0 ? (
           <p className="land-paragraph">
-            {`I am honored to be a guest on the traditional and unceded territories of the `}
+             {indigenousLandVariations[currentAcknowledgmentIndex]}
             <span className="indigenous-names">
               {indigenousLands.map((land, index) => (
                 <React.Fragment key={land.properties.Name}>
@@ -34,6 +52,9 @@ const LandAcknowledgment = ({ indigenousLands }) => {
         )}
       </div>
       <div className="cta">
+      <button onClick={changeAcknowledgment} className="cta-button">
+          Change Acknowledgment
+        </button>
         <button onClick={(e) => btnLearn(e)} className="cta-button">
           Learn More
         </button>
